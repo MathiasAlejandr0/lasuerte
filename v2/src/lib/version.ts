0715d0603d2c980a8105c116cec2561b1dd48bd6
Mathias <math.jaraa@duocuc.cl>
2026-08-23 14:18:47 -0400

    chore: apply admin credentials and environment variables

--- v2/src/lib/version.ts
+++ v2/src/lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "2.0.0";
-export const DEPLOY_TIMESTAMP = "2026-08-23T16:58:45Z";
+export const APP_VERSION = "2.0.1";
+export const DEPLOY_TIMESTAMP = "2026-08-23T18:18:40Z";
