df4b72f852c7b9e6a4909a1f336d36f828a10cce
Mathias <math.jaraa@duocuc.cl>
2026-08-23 21:11:23 -0400

    chore: deploy full environment variables for Flow, Supabase and Admin

--- v2/src/lib/version.ts
+++ v2/src/lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "2.0.1";
-export const DEPLOY_TIMESTAMP = "2026-08-23T18:18:40Z";
+export const APP_VERSION = "2.0.2";
+export const DEPLOY_TIMESTAMP = "2026-08-24T01:11:15Z";
